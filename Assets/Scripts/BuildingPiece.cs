16983090c22524d7c5e35160627ca8752889f9e9
David Tyler Edwards <davidtedwards2017@gmail.com>
2020-02-01 10:39:47 -0800

    update

---
+++ Assets/Scripts/BuildingPiece.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingPiece : MonoBehaviour
+{
+
+    //public Sprite[] Damage
+    //public float Damage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/GameStateController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MonsterLove.StateMachine;
+
+public class GameStateController : Singleton<GameStateController>
+{
+    public enum GameStates
+    {
+        init,
+        title,
+        intro,
+        playing,
+        finished,
+    }
+
+    public StateMachine<GameStates> statectrl;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        statectrl = StateMachine<GameStates>.Initialize(this);
+        statectrl.ChangeState(GameStates.init);
+    }
+
+    
+    public void init_Enter()
+    {
+
+    }
+
+    public void intro_Enter()
+    {
+
+    }
+
+    public void title_Enter()
+    {
+
+    }
+
+    public void playing_Enter()
+    {
+
+    }
+    
+    public void playing_Exit()
+    {
+
+    }
+
+    public void finished_Enter()
+    {
+
+    }
+}
