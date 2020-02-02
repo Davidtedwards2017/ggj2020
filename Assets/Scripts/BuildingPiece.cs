f5b97d1e9c2148e92e8c4b2677da3bc55234e0c8
cscandariato <cscandariato@gmail.com>
2020-02-02 14:49:47 -0800

    Updated music, sounds, more particlces

--- Assets/Scripts/BuildingPiece.cs
+++ Assets/Scripts/BuildingPiece.cs
@@ -15,6 +15,7 @@ public class BuildingPiece : MonoBehaviour
     [SerializeField] AudioClip[] crashClips;
     [SerializeField] AudioSource sfxSource;
     [SerializeField] AudioSource breakSource;
+    private float randomChance = 0;
 
 
     // Start is called before the first frame update
@@ -42,6 +43,19 @@ public class BuildingPiece : MonoBehaviour
         {
             sfxSource.PlayOneShot(crashClips[Random.Range(0, 8)]);
             //Debug.Log(col.relativeVelocity.magnitude);
+        }
+
+        else if (col.gameObject.tag == "Ground" && col.relativeVelocity.magnitude >= 4.0f)
+        {
+            //Debug.Log("Hit gronud!");
+            randomChance += Random.Range(0, 75);
+            if (randomChance >= 15)
+            {
+                Instantiate(SmallExplode, new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z), Quaternion.identity);
+                randomChance = 0;
+            }
+            else 
+                randomChance = 0;
         }
     }
 
